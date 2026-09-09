bd37d5d1a2a87977b5f998336fecb2b3201d4d1d
Sree-2007 <118925195+Sree-2007@users.noreply.github.com>
2026-09-09 20:37:53 +0530

    Update page.tsx

--- app/signal-display/[intersectionId]/page.tsx
+++ app/signal-display/[intersectionId]/page.tsx
@@ -1,9 +1,8 @@
 'use client';
-import dynamic from 'next/dynamic';
+import dynamicImport from 'next/dynamic';
 
-// We tell Next.js to never prerender this page
 export const dynamic = 'force-dynamic';
 
-const SignalDisplayPage = dynamic(() => import('./SignalDisplayPage'), { ssr: false });
+const SignalDisplayPage = dynamicImport(() => import('./SignalDisplayPage'), { ssr: false });
 
 export default SignalDisplayPage;
