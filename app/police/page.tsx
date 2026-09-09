a65ba08da3aa1a14e7b4b896f400362997e56bb0
Sree-2007 <118925195+Sree-2007@users.noreply.github.com>
2026-09-09 20:18:33 +0530

    Update page.tsx

--- app/police/page.tsx
+++ app/police/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Circle, useMapEvents } from 'react-leaflet';
